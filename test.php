43deb128ddd1421e1a5d4cf264a4177c835da166
Ebin <ebin.joseph@fingent.com>
2022-07-15 15:30:26 +0530

    FX:db

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "password", "test");
+$con = mysqli_connect("localhost", "root", "root", "test");
 //$con = mysqli_connect("localhost","my_user","my_password","my_db");
 
 // Check connection
